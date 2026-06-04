8368071e451bf4e776ba7f5e437e84ad0ded8e11
Eric Araújo <eric.araujo@gmail.com>
2026-06-04 15:53:49 -0400

    fix: use withastro/action@v6 and direct data imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: 'https://ericaraujo.com',
   integrations: [mdx()],
   output: 'static',
-  vite: {
-    resolve: {
-      alias: {
-        '@data': path.resolve(__dirname, './data'),
-      },
-    },
-  },
 });
